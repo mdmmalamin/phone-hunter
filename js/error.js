97df71d750d4859354b4c983f0aa613f818819f3
mdmmalamin <mdmmalamin@gmail.com>
2022-09-03 06:43:52 +0600

    modal done

--- js/error.js
+++ js/error.js
@@ -1,7 +1,16 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(res => res.json())
-.then(data => console.log(data))
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+// .then(data => console.log(data))
 
 // document.getElementById('abc');
 
 // synchronous asynchronous
+
+console.log(1);
+
+setTimeout(() => {
+    console.log(2);
+    console.log(3);
+}, 2500)
+
+console.log(4);
